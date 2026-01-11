75757c541138e85826b07111ad8a55b2e1d0a1b1
VTheGreek <vasilis.proefth@gmail.com>
2026-01-11 21:57:35 +0200

    Added Keyboard

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const display = document.getElementById("display");
 function handleButtonClick(evnt) {
   if (evnt.target.value === "AC") {
     display.value = "";
-  } else if (evnt.target.value === "C") {
+  } else if (evnt.target.value === "<-" || evnt.target.value === "C") {
     display.value = display.value.slice(0, -1);
 } else if (evnt.target.value === "=") {
     display.value = eval(display.value);
@@ -15,3 +15,22 @@ display.value += evnt.target.value;
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", handleButtonClick);
 }
+
+function handleKeyboardClick(evnt1) {
+  evnt1.preventDefault();
+  const allowedKeys = "0123456789+-=*/%."
+  if (evnt1.key === "Delete") {
+    display.value = "";
+    return;
+  } else if (evnt1.key === "Backspace") {
+    display.value = display.value.slice(0, -1);
+    return;
+  } else if (evnt1.key === "Enter") {
+    display.value = eval(display.value);
+    return;
+  } else if (!allowedKeys.includes(evnt1.key)){
+    return; 
+  } 
+    display.value += evnt1.key;
+}
+document.addEventListener("keydown", handleKeyboardClick);
